ab82cb6451a0ae12fed0cdcb8963412f058d9605
unknown <andrei24_vasilache@yahoo.com>
2026-06-06 20:51:23 +0300

    Initial project setup

---
+++ employee-onboarding-system-backend/src/test/java/com/comapny/employee_onboarding_system/EmployeeOnboardingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comapny.employee_onboarding_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeOnboardingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
